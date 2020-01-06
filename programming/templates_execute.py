0b8b4d611c2562a0c03d9fc42c16fde0f1d5ac3d
praveen poreddy <praveenporeddy8@gmail.com>
2020-01-06 10:26:15 -0600

    add these files

--- programming/templates_execute.py
+++ programming/templates_execute.py
@@ -3,6 +3,9 @@ from flask import Flask,render_template,request
 app=Flask(__name__)
 
 @app.route('/')
+def hello_world():
+    return 'hello,world!'
+'''@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -12,7 +15,7 @@ def user(name):
 
 @app.route('/details/<id>')
 def id_info(id):
-    return render_template('id.html',sub_id_info=id)
+    return render_template('id.html',sub_id_info=id)'''
 
 if __name__ == '__main__':
     app.run(debug=True)
